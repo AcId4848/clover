8b63cfa5f317c52a47ae7756ae45ac1f118c301c
AcId4848 <142092198+AcId4848@users.noreply.github.com>
2023-12-25 14:35:10 +0700

    Create clover_first_task.py

---
+++ clover_first_task.py
@@ -0,0 +1,33 @@
+import math
+import rospy
+from clover import srv
+from std_srvs.srv import Trigger
+
+rospy.init_node('flight')
+
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
+set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
+set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
+set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
+set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
+land = rospy.ServiceProxy('land', Trigger)
+
+
+def navigate_wait(x=0, y=0, z=1.5, speed=1, frame_id='aruco_map', tolerance=0.2, auto_arm=False):
+    navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
+
+    while not rospy.is_shutdown():
+        telem = get_telemetry(frame_id='navigate_target')
+        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
+            break
+        rospy.sleep(0.2)
+
+set_effect(r = '0', g = '255', b = '0')
+navigate_wait(frame_id='body', auto_arm=True)
+navigate_wait(4, 4, 2)
+navigate_wait(5, 5, 2)
+navigate_wait(5, 5 , 1)
+navigate_wait()
+land()
